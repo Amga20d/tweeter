a2d9593c68d4ff2172b644e531fbb43a35d1c986
Amgad Abdullah <amga200d@gmail.com>
2025-01-19 21:49:43 -0700

    form submission using ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,9 +1,3 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
 $(document).ready(function() {
   function createTweetElement(tweet) {
     const { user, content, created_at } = tweet;
@@ -32,6 +26,9 @@ $(document).ready(function() {
   }
 
   const renderTweets = function(tweets) {
+    // Clear existing tweets
+    $('#tweets-container').empty();
+
     // Loop through tweets
     tweets.forEach(tweet => {
       // Call createTweetElement for each tweet
@@ -44,141 +41,163 @@ $(document).ready(function() {
 
   // Fake data taken from initial-tweets.json
   const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
     },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
+    {
+      "user": {
+        "name": "Galileo",
+        "avatars": "https://i.imgur.com/5fUVPRP.png",
+        "handle": "@Galileo"
+      },
+      "content": {
+        "text": "E pur si muove"
+      },
+      "created_at": 1461120000000
     },
-    "content": {
-      "text": "Je pense , donc je suis"
+    {
+      "user": {
+        "name": "Einstein",
+        "avatars": "https://i.imgur.com/vKv8Cdl.png",
+        "handle": "@Albert"
+      },
+      "content": {
+        "text": "Imagination is more important than knowledge."
+      },
+      "created_at": 1461123600000
     },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Galileo",
-      "avatars": "https://i.imgur.com/5fUVPRP.png",
-      "handle": "@Galileo"
+    {
+      "user": {
+        "name": "Curie",
+        "avatars": "https://i.imgur.com/buD6iaE.png",
+        "handle": "@Marie"
+      },
+      "content": {
+        "text": "Nothing in life is to be feared, it is only to be understood."
+      },
+      "created_at": 1461127200000
     },
-    "content": {
-      "text": "E pur si muove"
+    {
+      "user": {
+        "name": "Turing",
+        "avatars": "https://i.imgur.com/8V9mlE6.png",
+        "handle": "@Alan"
+      },
+      "content": {
+        "text": "We can only see a short distance ahead, but we can see plenty there that needs to be done."
+      },
+      "created_at": 1461130800000
     },
-    "created_at": 1461120000000
-  },
-  {
-    "user": {
-      "name": "Einstein",
-      "avatars": "https://i.imgur.com/vKv8Cdl.png",
-      "handle": "@Albert"
+    {
+      "user": {
+        "name": "Hawking",
+        "avatars": "https://i.imgur.com/76HZPYM.png",
+        "handle": "@Stephen"
+      },
+      "content": {
+        "text": "Intelligence is the ability to adapt to change."
+      },
+      "created_at": 1461134400000
     },
-    "content": {
-      "text": "Imagination is more important than knowledge."
+    {
+      "user": {
+        "name": "Tesla",
+        "avatars": "https://i.imgur.com/4Hrm5hK.png",
+        "handle": "@Nikola"
+      },
+      "content": {
+        "text": "The present is theirs; the future, for which I really worked, is mine."
+      },
+      "created_at": 1461138000000
     },
-    "created_at": 1461123600000
-  },
-  {
-    "user": {
-      "name": "Curie",
-      "avatars": "https://i.imgur.com/buD6iaE.png",
-      "handle": "@Marie"
+    {
+      "user": {
+        "name": "Aristotle",
+        "avatars": "https://i.imgur.com/VlZO58J.png",
+        "handle": "@Aristotle"
+      },
+      "content": {
+        "text": "The more you know, the more you realize you don't know."
+      },
+      "created_at": 1461141600000
     },
-    "content": {
-      "text": "Nothing in life is to be feared, it is only to be understood."
+    {
+      "user": {
+        "name": "Faraday",
+        "avatars": "https://i.imgur.com/Jn29kC6.png",
+        "handle": "@Michael"
+      },
+      "content": {
+        "text": "But still try, for who knows what is possible!"
+      },
+      "created_at": 1461145200000
     },
-    "created_at": 1461127200000
-  },
-  {
-    "user": {
-      "name": "Turing",
-      "avatars": "https://i.imgur.com/8V9mlE6.png",
-      "handle": "@Alan"
+    {
+      "user": {
+        "name": "Lovelace",
+        "avatars": "https://i.imgur.com/NTpH3kg.png",
+        "handle": "@Ada"
+      },
+      "content": {
+        "text": "That brain of mine is something more than merely mortal, as time will show."
+      },
+      "created_at": 1461148800000
     },
-    "content": {
-      "text": "We can only see a short distance ahead, but we can see plenty there that needs to be done."
-    },
-    "created_at": 1461130800000
-  },
-  {
-    "user": {
-      "name": "Hawking",
-      "avatars": "https://i.imgur.com/76HZPYM.png",
-      "handle": "@Stephen"
-    },
-    "content": {
-      "text": "Intelligence is the ability to adapt to change."
-    },
-    "created_at": 1461134400000
-  },
-  {
-    "user": {
-      "name": "Tesla",
-      "avatars": "https://i.imgur.com/4Hrm5hK.png",
-      "handle": "@Nikola"
-    },
-    "content": {
-      "text": "The present is theirs; the future, for which I really worked, is mine."
-    },
-    "created_at": 1461138000000
-  },
-  {
-    "user": {
-      "name": "Aristotle",
-      "avatars": "https://i.imgur.com/VlZO58J.png",
-      "handle": "@Aristotle"
-    },
-    "content": {
-      "text": "The more you know, the more you realize you don't know."
-    },
-    "created_at": 1461141600000
-  },
-  {
-    "user": {
-      "name": "Faraday",
-      "avatars": "https://i.imgur.com/Jn29kC6.png",
-      "handle": "@Michael"
-    },
-    "content": {
-      "text": "But still try, for who knows what is possible!"
-    },
-    "created_at": 1461145200000
-  },
-  {
-    "user": {
-      "name": "Lovelace",
-      "avatars": "https://i.imgur.com/NTpH3kg.png",
-      "handle": "@Ada"
-    },
-    "content": {
-      "text": "That brain of mine is something more than merely mortal, as time will show."
-    },
-    "created_at": 1461148800000
-  },
-  {
-    "user": {
-      "name": "Darwin",
-      "avatars": "https://i.imgur.com/Tx3GT4b.png",
-      "handle": "@Charles"
-    },
-    "content": {
-      "text": "A man who dares to waste one hour of time has not discovered the value of life."
-    },
-    "created_at": 1461152400000
-  }
-];
-
+    {
+      "user": {
+        "name": "Darwin",
+        "avatars": "https://i.imgur.com/Tx3GT4b.png",
+        "handle": "@Charles"
+      },
+      "content": {
+        "text": "A man who dares to waste one hour of time has not discovered the value of life."
+      },
+      "created_at": 1461152400000
+    }
+  ];
 
   // Render tweets on page load
   renderTweets(data);
+
+  // Add event listener for form submission
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+
+    // Serialize the form data
+    const serializedData = $(this).serialize();
+    console.log('Serialized Data:', serializedData);
+    // Send the POST request using Ajax
+    $.ajax({
+      type: 'POST',
+      url: '/tweets/',
+      data: serializedData,
+      success: function(response) {
+        // Optional: Do something with the response, e.g., clear the form, show a message, etc.
+        console.log('Tweet successfully submitted:', response);
+      },
+      error: function(error) {
+        // Optional: Handle error, e.g., show an error message
+        console.error('Error submitting tweet:', error);
+      }
+    });
+  });
 });
